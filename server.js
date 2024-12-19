97fdcd27e965e0fa973a44cdcf7adefdc19d259f
Prakash <prakashpandey092001@gmail.com>
2024-12-20 00:27:57 +0530

    sorted task

--- server.js
+++ server.js
@@ -22,23 +22,14 @@ const Task = require('./models/Task');
 // API Routes
 app.get('/tasks', async (req, res) => {
     try {
-        const tasks = await Task.find();
+        const tasks = await Task.find().sort({ createdAt: -1 }); // Sort by createdAt in descending order
         res.json(tasks);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
     }
 });
 
-// app.post('/tasks', async (req, res) => {
-//     const { title, description } = req.body;
-//     const task = new Task({ title, description });
-//     try {
-//         const newTask = await task.save();
-//         res.status(201).json(newTask);
-//     } catch (err) {
-//         res.status(400).json({ message: err.message });
-//     }
-// });
 
 // Create a new task
 app.post('/tasks', async (req, res) => {
